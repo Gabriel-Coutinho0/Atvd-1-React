28868919ea492600da26c042367132a547234b71
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2023-04-09 20:34:26 -0300

    atualização atvd

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,8 @@
 // import { Contexto, Provider } from "../context/Contexto";
-import { useContext, useEffect, useState } from 'react';
-import { LoteriaProps } from '../types';
-import loteria from '../services/loteria';
-import trevoMega from '../images/trevo-megasena.png'
-import trevoLoto from '../images/trevo-lotofacil.png'
-import { Linha, LotoFacil, MegaSena } from "./sytle";
+import trevoMega from '../assets/trevo-megasena.png'
+import trevoLoto from '../assets/trevo-lotofacil.png'
+import '../styles/home.css'
+import { Linha, LotoFacil, MegaSena } from "../styles/sytle";
 import { useResultado } from '../context/Contexto';
 
 const Home = () => {
@@ -12,42 +10,70 @@ const Home = () => {
     // const { megasena, lotofacil } = useContext(Contexto)
 
     const { resultado } = useResultado();
+    let qtd;
+    function verficarGanhadores() {
+        if (resultado.megasena.quantidadeGanhadores === 1) {
+            qtd = " ganhador"
+        }
+        else {
+            qtd = " ganhadores"
+        }
+        return qtd
+    }
     return (
-        <div>
-            <div >
-                <div>
-                    {/* <Provider>
-                        {megasena ? (<p>{megasena.dataPorExtenso}</p>):<div></div>} */}
-                    {resultado.megasena && (
-                        <p>{resultado.megasena.acumulado}</p>
-                    )}
-                    <div className=''>
-                        <Linha>
-                            <img src={trevoMega}></img>
-                            <MegaSena>Mega-Sena</MegaSena>
-                            {resultado.megasena.acumulado ? "ACUMULADO" : ""}
-
-                        </Linha>
-                        <LotoFacil>Lotofácil</LotoFacil>
-                        <img src={trevoLoto}></img>
-
-
-                        {/*
-                            <p>MEGA SENA</p> */}
-                        {/* <p> Estimativa de prêmio do proximo concurso. Sorteio em {response.megasena.dataProximoConcurso}:</p> */}
-                        {/* <p> R$ {megasena.dataApuracao}</p> */}
+        <div className='form'>
+            <div className='container'>
+                {resultado.megasena && (
+                    <p>{resultado.megasena.acumulado}</p>
+                )}
+                <div className='coluna'>
+                    <div className='header-container'>
+                        <div className='imagem-header'><img src={trevoMega}></img></div>
+                        <div className='titulo-mega'><h3 >MEGA-SENA</h3></div>
                     </div>
+                    <div className='descricao-mega'>
+                        Estimativa do prêmio do proximo concurso. Sorteio {resultado.megasena.dataProximoConcurso}:
+                    </div>
+                    <div className='mega-valor'>
+                        <h2>{resultado.megasena.valorEstimadoProximoConcurso.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</h2>
+                        {/* megaSena.valorEstimadoProximoConcurso.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }):"" */}
+                        {/* {resultado.megasena.acumulado ? "ACUMULADO" : ""} */}
+                    </div>
+                </div>
+                <div className='coluna'>
+                    <div className="coluna-dezenas">
+                        <ul>{resultado.megasena.dezenas ? resultado.megasena.dezenas.map((elementos) =>
+                            <li>
+                                {elementos}
+                            </li>
+                        ) : ""}</ul>
+                    </div>
+                    <div className='vencedores-mega'>
+                        <p>{resultado.megasena.acumulado ? "ACUMULADO" : resultado.megasena.quantidadeGanhadores + verficarGanhadores()}</p>
+                    </div>
+                    <div className='concurso-mega'>
+                    <p>Concurso {resultado.megasena.numeroDoConcurso}- {resultado.megasena.dataPorExtenso}</p>
+                    </div>
+                </div>
+            </div>
+            <Linha></Linha>
+            <div className='container-2'>
+                <div className='coluna-2'>
+                    <LotoFacil><img src={trevoLoto}></img> LOTOFÁCIL</LotoFacil>
+                    <p>Estimativa de prêmio do proximo concurso. Sorteio em {resultado.lotofacil.dataProximoConcurso}:</p>
                     <div>
-                        {/* <p> {response.megasena.dezenas}</p> */}
-                        {/* <p>ACUMULADO arrumar {response.megasena.acumulado}</p> */}
-                        {/* <p> Concurso {response.megasena.numeroDoConcurso} - {response.megasena.dataPorExtenso}</p> */}
+                    <p>R$: {resultado.lotofacil.valorEstimadoProximoConcurso}</p>
                     </div>
-
-                    {/* </Provider> */}
+                </div>
+                <div className='coluna-2'>
+                    <p>{resultado.lotofacil.dezenas}</p>
+                    <p>{resultado.lotofacil.quantidadeGanhadores} GANHADORES</p>
+                    <p>Concurso {resultado.lotofacil.numeroDoConcurso}- {resultado.lotofacil.dataPorExtenso}</p>
 
                 </div>
-
             </div>
+
+
         </div>
     );
 }
